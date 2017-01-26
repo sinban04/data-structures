55fee1d82ad6cb0ce60ea678120bf120b0261c17
Injung Hwang <sinban04@gmail.com>
2017-01-27 02:34:50 +0900

    In the working of stack

--- stack/stack.h
+++ stack/stack.h
@@ -5,7 +5,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* maximum stack size */
+#define MAX_STACK_SIZE 100
+
+typedef struct {
+  int key;
+  void* item;
+}element_t;
+
+element_t g_stack[MAX_STACK_SIZE];
 int top=-1;
+
+bool is_empty(element_t* stack){
+  return (stack) ? true : false;
+}
+
+bool is_full(element_t* stack){
+  return (top==MAX_STACK_SIZE-1) ? true : false;
+}
+
+void push(element_t* element){
+  if(is_full(g_stack) == true){
+    fprintf(stderr, "Stack is full!\n");
+  }
+  
+  g_stack[++top] = element;
+}
+element_t pop(){
+  if(top == -1){
+    fprintf(stderr, "Stcak is empty!\n");
+    return NULL;
+  }
+  return g_stack[top--];
+}
+
 
 
 
